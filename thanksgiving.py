3fe556ea1e7e3ff587423c00649a7f35fa77e916
Miles Winter <milesjwinter@system76-pc.localdomain>
2020-11-24 12:55:33 -0600

    change file type

--- thanksgiving.py
+++ thanksgiving.py
@@ -59,4 +59,5 @@ events = [
 dinner_time = datetime(2020, 11, 26, hour=14)
 events = timeline(event_time=dinner_time, events=events)
 g = graph_timeline(events)
-g.view()
+g.format = "png"
+g.render("outputs/timeline-graph.gv", view=True)
