70a9802e4eac007eb632fd06a04de471e0dcaf80
Miles Winter <milesjwinter@system76-pc.localdomain>
2020-11-25 09:18:32 -0600

    add support for start time

--- timeline/timeline.py
+++ timeline/timeline.py
@@ -1,22 +1,32 @@
 from datetime import datetime, timedelta
-from typing import Sequence
+from typing import Literal, Sequence
 
 from timeline.event import Event
 
 
 def timeline(
-    event_time: datetime, events: Sequence[Event], verbose: bool = True
+    events: Sequence[Event],
+    event_time: datetime,
+    event_time_type: Literal["start", "end"] = "end",
+    verbose: bool = True,
 ) -> Sequence[Event]:
-    """construct timeline based on specified event durations. Most
-    useful when you don't know the absolute event times. Events are
-    assumed to take place prior to `event_time`
+    """construct timeline based on specified event durations. Only
+    useful when you don't know the absolute event times. Note: this
+    function assigns the `event_time` attribute of the input event
+    sequence
 
     Parameters
     ----------
-    event_time: datetime
-        time event takes place
     events: Sequence[Event]
         time ordered sequence of events leading up to `event_time`
+    event_time: datetime
+        time event takes place
+    event_time_type: str, one of "start" or "end", default = "end"
+        literal indicating whether `event_time` is at the beginning of the
+        sequence or the end. For example, "start" should be used to answer
+        the question, if I start prep at `event_time` when will dinner be?
+        Similarly, "end" should be used to answer the question, if I want to
+        eat dinner at `event_time` when should I begin prep/cooking
     verbose: bool, default = True
         print timeline to stdout
 
@@ -24,11 +34,26 @@ def timeline(
     -------
     events: Sequence[Event]
         time ordered sequence of events w.r.t. `event_time`
+
+    Raises
+    ------
+    AssertionError
     """
+    assert event_time_type in [
+        "start",
+        "end",
+    ], "event_time_type must be 'start' or 'end'"
+
     temp_time = timedelta(seconds=0)
-    for event in reversed(events):
-        temp_time += event.duration
-        event.event_time = event_time - temp_time
+    if event_time_type == "end":
+        for event in reversed(events):
+            temp_time += event.duration
+            event.event_time = event_time - temp_time
+
+    if event_time_type == "start":
+        for event in events:
+            temp_time += event.duration
+            event.event_time = event_time + temp_time
 
     if verbose:
         for event in events:
